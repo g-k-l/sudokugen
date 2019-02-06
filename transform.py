4646c892bf1ab42f6917b8fcfebf5c13017ef16f
Kevin Liu <gkliu93@msn.com>
2019-02-05 22:46:57 -0800

    add skeleton implementation for transforms

---
+++ transform.py
@@ -0,0 +1,45 @@
+"""
+Defines transformation which would create
+perceptually different puzzles from each
+existing puzzle.
+"""
+import numpy as np
+
+from constants import COMPLETE_ROW, BOARD_DIM
+
+
+def x_translate(times=1):
+    pass
+
+
+def y_translate(times=1):
+    pass
+
+
+def rotate(times=1):
+    pass
+
+
+def mirror_x():
+    pass
+
+
+def mirror_y():
+    pass
+
+
+def mirror_major_diagonal():
+    pass
+
+
+def mirror_minor_diagonal():
+    pass
+
+
+def shuffle_numbers():
+    """
+    Produce 9!-1 (362879) equivalent
+    puzzles from the original by mapping
+    each number to a different number.
+    """
+    pass
