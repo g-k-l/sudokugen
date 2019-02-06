a96abc1b360df81189771ff9426e56e7699ec8cd
Kevin Liu <gkliu93@msn.com>
2019-02-06 05:16:52 -0800

    implemented transforms with tests

--- transform.py
+++ transform.py
@@ -5,41 +5,46 @@ existing puzzle.
 """
 import numpy as np
 
-from constants import COMPLETE_ROW, BOARD_DIM
+from constants import BOARD_DIM
 
 
-def x_translate(times=1):
-    pass
+def x_translate(board, times=1):
+    return np.roll(board, shift=times*3, axis=0)
 
 
-def y_translate(times=1):
-    pass
+def y_translate(board, times=1):
+    return np.roll(board, shift=times*3, axis=1)
 
 
-def rotate(times=1):
-    pass
+def rotate(board, times=1):
+    return np.rot90(board, k=times)
 
 
-def mirror_x():
-    pass
+def mirror_x(board):
+    return np.flipud(board)
 
 
-def mirror_y():
-    pass
+def mirror_y(board):
+    return np.fliplr(board)
 
 
-def mirror_major_diagonal():
-    pass
+def mirror_major_diagonal(board):
+    raise NotImplementedError("Equivalent to rotate + mirror.")
 
 
-def mirror_minor_diagonal():
-    pass
+def mirror_minor_diagonal(board):
+    raise NotImplementedError("Equivalent to rotate + mirror.")
 
 
-def shuffle_numbers():
+def shuffle_numbers(board):
     """
-    Produce 9!-1 (362879) equivalent
+    Produce one of 9!-1 (362879) equivalent
     puzzles from the original by mapping
     each number to a different number.
     """
-    pass
+    num_map = np.arange(BOARD_DIM)
+    np.random.shuffle(np.arange(BOARD_DIM))
+    copy = -board
+    for i, n in enumerate(num_map):
+        copy[copy == -(i+1)] = n + 1
+    return copy
