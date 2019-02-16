a793b83587132e7a9b419aae9d53e2a1902d0418
Kevin Liu <gkliu93@msn.com>
2019-02-16 08:01:31 -0800

    added more options

--- sudokugen/__main__.py
+++ sudokugen/__main__.py
@@ -1,11 +1,21 @@
 import sys
-
-from .gen_sol import main
+import numpy as np
+from .gen_sol import main, backtrack_iter, create_puzzle_from_board
 from .client import client
 
 if __name__ == "__main__":
     if sys.argv[1] == "play":
         sys.exit(client() or 0)     
+    elif sys.argv[1] == "solve":
+        board = np.array([int(c) for c in sys.argv[2]]).reshape(9, 9)
+        solution = backtrack_iter(board)
+        print(solution)
+        sys.exit(0)
+    elif sys.argv[1] == "gen":
+        board = np.zeros(81).reshape(9, 9)
+        puzzle = create_puzzle_from_board(backtrack_iter(board))
+        print(puzzle)
+        sys.exit(0)
     else:
         try:
             n_jobs = int(sys.argv[1])
