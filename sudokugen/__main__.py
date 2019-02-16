947cf872d1e17fe34dc3eb8a0e8084920f8397df
Kevin Liu <gkliu93@msn.com>
2019-02-16 03:35:46 -0800

    add entry point for curses app

--- sudokugen/__main__.py
+++ sudokugen/__main__.py
@@ -1,11 +1,14 @@
 import sys
 
 from .gen_sol import main
+from .client import client
 
 if __name__ == "__main__":
-    try:
-        n_jobs = int(sys.argv[1])
-    except (IndexError, ValueError):
-        n_jobs = None 
-
-    sys.exit(main(n_jobs) or 0)
+    if sys.argv[1] == "play":
+        sys.exit(client() or 0)     
+    else:
+        try:
+            n_jobs = int(sys.argv[1])
+        except (IndexError, ValueError):
+            n_jobs = None 
+        sys.exit(main(n_jobs) or 0)
