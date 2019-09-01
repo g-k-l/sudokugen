47a843a9d9db6e9dd15881cce3485cad4b20c01e
Kevin Liu <gkliu93@msn.com>
2019-09-01 02:57:24 -0700

    improve checks with tests

--- sudokugen/tests.py
+++ sudokugen/tests.py
@@ -15,15 +15,71 @@ from . import transform as tf
 @pytest.fixture
 def board():
     return np.array(
-        [[1, 3, 5, 2, 7, 8, 9, 6, 4],
-         [6, 8, 2, 4, 9, 5, 7, 1, 3],
-         [9, 7, 4, 3, 1, 6, 5, 2, 8],
-         [2, 6, 1, 9, 4, 7, 8, 3, 5],
-         [3, 5, 7, 6, 8, 2, 4, 9, 1],
-         [8, 4, 9, 1, 5, 3, 6, 7, 2],
-         [7, 2, 3, 5, 6, 4, 1, 8, 9],
-         [5, 9, 6, 8, 3, 1, 2, 4, 7],
-         [4, 1, 8, 7, 2, 9, 3, 5, 6]])
+      [[1, 3, 5, 2, 7, 8, 9, 6, 4],
+       [6, 8, 2, 4, 9, 5, 7, 1, 3],
+       [9, 7, 4, 3, 1, 6, 5, 2, 8],
+       [2, 6, 1, 9, 4, 7, 8, 3, 5],
+       [3, 5, 7, 6, 8, 2, 4, 9, 1],
+       [8, 4, 9, 1, 5, 3, 6, 7, 2],
+       [7, 2, 3, 5, 6, 4, 1, 8, 9],
+       [5, 9, 6, 8, 3, 1, 2, 4, 7],
+       [4, 1, 8, 7, 2, 9, 3, 5, 6]], int)
+
+
+@pytest.fixture
+def invalid_board():
+    return np.array(
+      [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 1, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 1, 0, 0, 0, 0, 0, 0],
+       [0, 4, 0, 0, 0, 0, 0, 0, 0],
+       [0, 9, 0, 0, 0, 0, 0, 0, 0],
+       [0, 7, 0, 7, 0, 0, 0, 3, 0],
+       [0, 5, 0, 0, 0, 0, 0, 0, 0],
+       [0, 6, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 3, 0]], int)
+
+
+@pytest.fixture
+def corrupt_board():
+    return np.array(
+      [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 1, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 1, 0, 0, 0, 0, 0, 0],
+       [0, 4, 0, 0, -999, 0, 0, 0, 0],
+       [0, 9, 0, 0, 0, 0, 0, 0, 0],
+       [0, 7, 0, 100, 0, 0, 0, 3, 0],
+       [0, 5, 0, 0, 0, 0, 0, 0, 0],
+       [0, 6, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 3, 0]], int)
+
+
+@pytest.fixture
+def easy_board():
+    return np.array(
+      [[0, 3, 6, 8, 9, 2, 7, 1, 5],
+       [5, 0, 2, 0, 7, 1, 9, 0, 3],
+       [9, 0, 7, 5, 6, 3, 4, 8, 2],
+       [0, 4, 3, 1, 5, 8, 2, 0, 7],
+       [8, 5, 9, 6, 0, 7, 1, 3, 0],
+       [7, 2, 0, 9, 3, 4, 8, 5, 6],
+       [0, 0, 0, 2, 8, 6, 5, 0, 1],
+       [0, 0, 0, 3, 1, 0, 0, 4, 9],
+       [0, 0, 0, 7, 4, 9, 3, 2, 8]], int)
+
+
+@pytest.fixture
+def hard_board():
+    return np.array(
+      [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 1, 0, 0, 0, 0, 0, 0, 0],
+       [0, 2, 0, 0, 0, 0, 0, 0, 0],
+       [0, 4, 0, 0, 0, 0, 0, 0, 0],
+       [0, 9, 0, 0, 0, 0, 0, 0, 0],
+       [0, 7, 0, 0, 0, 0, 0, 3, 0],
+       [0, 5, 0, 0, 0, 0, 0, 0, 0],
+       [0, 6, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0]], int)
 
 
 def test_squares_group():
@@ -34,22 +90,30 @@ def test_squares_group():
     assert (4, 8) in groups[5]
 
 
-def test_squares_choices():
-    board = np.array(
-      [[0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 1, 0, 0, 0, 0, 0, 0, 0],
-       [0, 2, 0, 0, 0, 0, 0, 0, 0],
-       [0, 4, 0, 0, 0, 0, 0, 0, 0],
-       [0, 9, 0, 0, 0, 0, 0, 0, 0],
-       [0, 7, 0, 0, 0, 0, 0, 3, 0],
-       [0, 5, 0, 0, 0, 0, 0, 0, 0],
-       [0, 6, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0]])
-
+def test_squares_choices(hard_board):
+    board = hard_board
     choices = gen_sol.choices_from_square
     assert choices(board, 8, 1) == {1, 2, 3, 4, 7, 8, 9}
     assert choices(board, 0, 7) == {1, 2, 3, 4, 5, 6, 7, 8, 9}
     assert gen_sol.construct_candidates(board, 8, 1) == {3, 8}
+
+
+def test_assert_board_is_valid(board, invalid_board, corrupt_board, easy_board, hard_board):
+    assert gen_sol.assert_board_is_valid(board)
+    assert gen_sol.assert_board_is_valid(easy_board)
+    assert gen_sol.assert_board_is_valid(hard_board)
+    assert not gen_sol.assert_board_is_valid(BLOCK_ARRAY)
+    assert not gen_sol.assert_board_is_valid(invalid_board)
+    assert not gen_sol.assert_board_is_valid(corrupt_board)
+
+
+def test_assert_sol_is_valid(board, invalid_board, corrupt_board, easy_board, hard_board):
+    assert gen_sol.assert_sol_is_valid(board)
+    assert not gen_sol.assert_sol_is_valid(easy_board)
+    assert not gen_sol.assert_sol_is_valid(hard_board)
+    assert not gen_sol.assert_sol_is_valid(BLOCK_ARRAY)
+    assert not gen_sol.assert_sol_is_valid(invalid_board)
+    assert not gen_sol.assert_sol_is_valid(corrupt_board)
 
 
 def test_get_unfilled_cell():
@@ -64,52 +128,43 @@ def test_get_unfilled_cell_all_filled():
         gen_sol.get_unfilled_cell_rand(board)
 
 @pytest.mark.timeout(5, "too slow", method="thread")
-def test_backtrack_iter():
+def test_backtrack_iter(easy_board):
     # use a partially solved board so that the tests
     # do not run for too long.
-    board = np.array(
-      [[0, 3, 6, 8, 9, 2, 7, 1, 5],
-       [5, 0, 2, 0, 7, 1, 9, 0, 3],
-       [9, 0, 7, 5, 6, 3, 4, 8, 2],
-       [0, 4, 3, 1, 5, 8, 2, 0, 7],
-       [8, 5, 9, 6, 0, 7, 1, 3, 0],
-       [7, 2, 0, 9, 3, 4, 8, 5, 6],
-       [0, 0, 0, 2, 8, 6, 5, 0, 1],
-       [0, 0, 0, 3, 1, 0, 0, 4, 9],
-       [0, 0, 0, 7, 4, 9, 3, 2, 8]], int)
+    board = easy_board
     # board = np.zeros((BOARD_DIM, BOARD_DIM,), dtype=int)
     sol = gen_sol.backtrack_iter(board)
     assert len(np.argwhere(sol == 0)) == 0
-    gen_sol.assert_board_is_valid(sol)
+    gen_sol.assert_sol_is_valid(sol)
 
 
 def test_x_translate(board):
     assert np.all(np.equal(tf.x_translate(board, times=3), board))
     for n in range(3):
-        gen_sol.assert_board_is_valid(tf.x_translate(board, times=n+1))
+        gen_sol.assert_sol_is_valid(tf.x_translate(board, times=n+1))
 
 
 def test_y_translate(board):
     assert np.all(np.equal(tf.y_translate(board, times=3), board))
     for n in range(3):
-        gen_sol.assert_board_is_valid(tf.y_translate(board, times=n+1))
+        gen_sol.assert_sol_is_valid(tf.y_translate(board, times=n+1))
 
 
 def test_rotate(board):
     for n in range(3):
-        gen_sol.assert_board_is_valid(tf.rotate(board, times=n+1))
+        gen_sol.assert_sol_is_valid(tf.rotate(board, times=n+1))
 
 
 def test_mirror_x(board):
-    gen_sol.assert_board_is_valid(tf.mirror_x(board))
+    gen_sol.assert_sol_is_valid(tf.mirror_x(board))
 
 
 def test_mirror_y(board):
-    gen_sol.assert_board_is_valid(tf.mirror_y(board))
+    gen_sol.assert_sol_is_valid(tf.mirror_y(board))
 
 
 def test_shuff_numbers(board):
-    gen_sol.assert_board_is_valid(tf.shuffle_numbers(board))
+    gen_sol.assert_sol_is_valid(tf.shuffle_numbers(board))
 
 
 def test_candidates_dict(board):
