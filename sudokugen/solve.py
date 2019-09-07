2461ec284ac96ff80d5a81c1874d913cffa81463
Kevin Liu <gkliu93@msn.com>
2019-09-06 19:57:46 -0700

    add doctests

--- sudokugen/solve.py
+++ sudokugen/solve.py
@@ -21,30 +21,58 @@ def empty_puzzle():
 
 
 def block(k):
-    """Given a cell label (0-81), return
-    the 3x3 block for which it is in
-        """
+    """Given a cell label (0-80), return
+    the 3x3 block for which it is in.
+
+    >>> block(3)
+    1
+    >>> block(72)
+    6
+    """
     r, c = (k // DIM), (k % DIM)
-    return (r // 3) + 3*(c // 3)
+    return 3*(r // 3) + (c // 3)
 
 
 def block_indices():
-    """Returns a dict where keys are block labels
-        (int from 0-8) and vals are the cell labels
-        that belong to the block"""
+    """
+    Returns a dict where keys are block labels
+    (int from 0-8) and vals are the cell labels
+    that belong to the block.
+
+    >>> indices = block_indices()[0]
+    >>> indices == {0, 1, 2, 9, 10, 11, 18, 19, 20}
+    True
+    >>> indices = block_indices()[4]
+    >>> indices == {30, 31, 32, 39, 40, 41, 48, 49, 50}
+    True
+    """
     blocks = defaultdict(set)
     for k in range(SIZE):
         r, c = (k // DIM), (k % DIM)
-        b = (r // 3) + 3*(c // 3)
+        b = block(k)
         blocks[b].add(k)
     # remove the default property
     return dict(blocks)
 
 
 def indices():
-    """Returns two dicts, where the keys are
-        row/col labels and the vals are cell labels
-        which belong to the corresponding row/col."""
+    """
+    Returns two dicts, where the keys are
+    row/col labels and the vals are cell labels
+    which belong to the corresponding row/col.
+
+    >>> rows, cols = indices()
+    >>> rows[0] == {0, 1, 2, 3, 4, 5, 6, 7, 8}
+    True
+    >>> rows[4] == {36, 37, 38, 39, 40, 41, 42, 43, 44}
+    True
+    >>> cols[2] == {2, 11, 20, 29, 38, 47, 56, 65, 74}
+    True
+    >>> cols[9]
+    Traceback (most recent call last):
+    ...
+    KeyError: 9
+    """
     rows = defaultdict(set)
     cols = defaultdict(set)
     for k in range(SIZE):
@@ -81,13 +109,24 @@ class NoSolution(SudokuBaseException):
 
 
 def assign(cands_of, k, val):
-    """Assign val to k by eliminating
-        all other_vals in cands_of[k]"""
+    """
+    Assign val to k by eliminating all other_vals in cands_of[k]
+
+    >>> cands_of = {k: set(COMPLETE_ROW) for k in range(SIZE)}
+    >>> assign(cands_of, 0, 1)
+    >>> cands_of[0] == {1}
+    True
+    >>> cands_of[10] == {2, 3, 4, 5, 6, 7, 8, 9} # same block
+    True
+    >>> cands_of[8] == {2, 3, 4, 5, 6, 7, 8, 9} # same col
+    True
+    >>> cands_of[1] == {2, 3, 4, 5, 6, 7, 8, 9} # same row
+    True
+    """
     for other_val in COMPLETE_ROW:
         if other_val == val:
             continue
         eliminate(cands_of, k, other_val)
-    return cands_of
 
 
 def eliminate(cands_of, k, val):
@@ -104,9 +143,15 @@ def eliminate(cands_of, k, val):
         values for the cell.
     :k: the cell label (int)
     :val: the val to eliminate from cands_of[k] (int)
+
+    # trivial example:
+    >>> cands_of = {k: set(COMPLETE_ROW) for k in range(SIZE)}
+    >>> eliminate(cands_of, 0, 1)
+    >>> cands_of[0] == {2, 3, 4, 5, 6, 7, 8, 9}
+    True
     """
     if val not in cands_of[k]:
-        return cands_of
+        return
 
     cands_of[k].remove(val)
     if len(cands_of[k]) == 0:
@@ -115,7 +160,7 @@ def eliminate(cands_of, k, val):
         (val2,) = cands_of[k]
         for p in PEERS_OF[k]:
             eliminate(cands_of, p, val2)
-        return cands_of
+        return
 
     for unit in UNITS_OF[k]:
         # location in unit where val is possible
@@ -125,8 +170,7 @@ def eliminate(cands_of, k, val):
         elif len(locs) == 1:
             (loc,) = locs
             assign(cands_of, loc, val)
-            return cands_of
-    return cands_of
+            return
 
 
 def init_cands_of(puzzle):
@@ -138,6 +182,8 @@ def init_cands_of(puzzle):
     of this function (usually) leads to a complete solution.
 
     :puzzle: a list of ints, each of which is from 0 to 9
+
+    >>> puzzle = []
     """
     cands_of = {k: set(COMPLETE_ROW) for k in range(SIZE)}
     for k, val in enumerate(puzzle):
@@ -184,3 +230,7 @@ def solve(puzzle):
                 else:
                     stack.append(next_cands_of)
     raise NoSolution("contradiction")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
