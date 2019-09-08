6bc968e75143de51003a51966f27742a61f01210
Kevin Liu <gkliu93@msn.com>
2019-09-07 17:49:46 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(name='sudokugen',
       python_requires=">=3.6.9",
-      version='0.2',
+      version='0.2.1',
       description='A sudoku puzzle solver and generator',
-      url='http://github.com/g-k-l/sudoku-gen',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/g-k-l/sudokugen',
       author='Kevin Liu',
       author_email='kevin.g.k.liu@gmail.com',
       license='GNUPL',
-      packages=['sudokugen'],
+      packages=find_packages(),
       zip_safe=True,
       )
