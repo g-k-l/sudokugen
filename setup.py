d11bf1b498c8c5c70b1e9a94920845aaa4d95b80
Kevin Liu <gkliu93@msn.com>
2019-09-07 17:22:57 -0700

    enforce python version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name='sudokugen',
+      python_requires=">=3.6.9",
       version='0.2',
       description='A sudoku puzzle solver and generator',
       url='http://github.com/g-k-l/sudoku-gen',
